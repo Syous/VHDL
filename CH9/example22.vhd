9a859c13a1f55c52d35050c3e578672ca190da6b
Syous <andhenry87@gmailcom>
2023-04-06 23:22:03 -0400

    Modified example22 to add the xnor and and gate port maps

--- CH9/example22.vhd
+++ CH9/example22.vhd
@@ -11,6 +11,35 @@ entity example22_3bitcomp is
 end entity example22_3bitcomp;
 
 architecture arch_3bitcomp of example22_3bitcomp is
+    
+    --XNOR component    
+    component example22_xnor is
+        port
+        (
+            A_IN, B_IN  :   in std_logic;
+            F_OUT       :   out std_logic
+        );
+
+    end component example22_xnor;
+    
+    -- AND component
+    component example22_and is
+        port
+        (
+            A_IN, B_IN, C_IN    :   in std_logic;
+            F_OUT               :   out std_logic 
+        );
+    end component example22_and;
+    
+    signal xnor1_out, xnor2_out, xnor3_out  :   std_logic;
+
     begin
-        -- testing
+        
+        -- create the 3 individual xnor's for each input in this comparator
+        xnor1   :   example22_xnor  port map    (A_IN(0), B_IN(0), xnor1_out);
+        xnor2   :   example22_xnor  port map    (A_IN(1), B_IN(1), xnor2_out);
+        xnor3   :   example22_xnor  port map    (A_IN(2), B_IN(2), xnor3_out);
+        
+        -- create the 3-input and gate using the output signals from the xnors
+        and3    :   example22_and   port map    (xnor1_out, xnor2_out, xnor3_out, EQ_OUT);
 end architecture arch_3bitcomp;
