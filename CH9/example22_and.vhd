8717c7a0150f2f421425a6735dec5439203a90fc
Syous <andhenry87@gmailcom>
2023-04-06 23:23:32 -0400

    Added C_IN and modified the and gate

--- CH9/example22_and.vhd
+++ CH9/example22_and.vhd
@@ -4,11 +4,11 @@ use ieee.std_logic_1164.all;
 entity two_bit_and is
     port
     (
-        A_IN, B_IN  :   in std_logic;
-        F_OUT       :   out std_logic
+        A_IN, B_IN, C_IN    :   in std_logic;
+        F_OUT               :   out std_logic
     );
 end entity two_bit_and;
 
 architecture rtl of two_bit_and is
-    F_OUT   <=  A_IN and B_IN;
+    F_OUT   <=  A_IN and B_IN and C_IN;
 end architecture rtl;
